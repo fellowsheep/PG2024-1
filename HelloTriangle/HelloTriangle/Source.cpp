3855dcd0d2125de88262f53cff74b5ee711e69d1
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2024-03-18 22:10:21 -0300

    Adicionada correção do exercicio 8 (Lista 1) em HelloTriangle

--- HelloTriangle/HelloTriangle/Source.cpp
+++ HelloTriangle/HelloTriangle/Source.cpp
@@ -21,7 +21,8 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 // Protótipos das funções
 int setupGeometry();
-int setupGeometry2();
+int exercicio5();
+int exercicio8();
 
 // Dimensões da janela (pode ser alterado em tempo de execução)
 const GLuint WIDTH = 800, HEIGHT = 600;
@@ -72,15 +73,17 @@ int main()
 
 
 	// Compilando e buildando o programa de shader
-	Shader shader("../shaders/helloTriangle.vs", "../shaders/helloTriangle.fs");
+	//Shader shader("../shaders/helloTriangle.vs", "../shaders/helloTriangle.fs");
+	Shader shader("../shaders/exercicio8.vs", "../shaders/exercicio8.fs");
 
 	// Gerando um buffer simples, com a geometria de um triângulo
-	GLuint VAO = setupGeometry2();
+	//GLuint VAO = exercicio5();
+	GLuint VAO = exercicio8();
 	
 	// Enviando a cor desejada (vec4) para o fragment shader
 	// Utilizamos a variáveis do tipo uniform em GLSL para armazenar esse tipo de info
 	// que não está nos buffers
-	GLint colorLoc = glGetUniformLocation(shader.ID, "inputColor");
+	//GLint colorLoc = glGetUniformLocation(shader.ID, "inputColor");
 	
 	shader.Use();
 	
@@ -99,14 +102,20 @@ int main()
 
 		glBindVertexArray(VAO); //Conectando ao buffer de geometria
 
-		glUniform4f(colorLoc, 0.0f, 0.0f, 1.0f, 1.0f); //enviando cor para variável uniform inputColor
+		//1 - Polígono Preenchido
 
-		// Chamada de desenho - drawcall
-		// Poligono Preenchido - GL_TRIANGLES
-		// Vértices - GL_POINTS
+		//glUniform4f(colorLoc, 0.0f, 0.0f, 1.0f, 1.0f); //enviando cor para variável uniform inputColor
+		//shader.setVec4("inputColor", 0.0f, 0.0f, 1.0f, 1.0f);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		//2 - Polígono com contorno
+		//shader.setVec4("inputColor", 0.0f, 1.0f, 1.0f, 1.0f); //magenta
 		glDrawArrays(GL_LINE_LOOP, 0, 3);
-		glUniform4f(colorLoc, 0.0f, 1.0f, 1.0f, 1.0f);
 		glDrawArrays(GL_LINE_LOOP, 3, 3);
+
+		//3 - Apenas os vértices dos polígonos
+		//shader.setVec4("inputColor", 1.0f, 1.0f, 0.0f, 1.0f);
+		glDrawArrays(GL_POINTS, 0, 6);
 
 		glBindVertexArray(0); //Desconectando o buffer de geometria
 
@@ -181,7 +190,7 @@ int setupGeometry()
 	return VAO;
 }
 
-int setupGeometry2()
+int exercicio5()
 {
 	// Aqui setamos as coordenadas x, y e z do triângulo e as armazenamos de forma
 	// sequencial, já visando mandar para o VBO (Vertex Buffer Objects)
@@ -206,7 +215,7 @@ int setupGeometry2()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//Envia os dados do array de floats para o buffer da OpenGl
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+	//                                              vertices.data()  
 	//Geração do identificador do VAO (Vertex Array Object)
 	glGenVertexArrays(1, &VAO);
 	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
@@ -221,6 +230,63 @@ int setupGeometry2()
 	// Deslocamento a partir do byte zero 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+
+	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
+	// atualmente vinculado - para que depois possamos desvincular com segurança
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+	glBindVertexArray(0);
+
+	return VAO;
+}
+
+int exercicio8()
+{
+	// Aqui setamos as coordenadas x, y e z do triângulo e as armazenamos de forma
+	// sequencial, já visando mandar para o VBO (Vertex Buffer Objects)
+	// Cada atributo do vértice (coordenada, cores, coordenadas de textura, normal, etc)
+	// Pode ser arazenado em um VBO único ou em VBOs separados
+	GLfloat vertices[] = {
+		//x   y     z    r   g    b
+		//Triangulo 0
+		-0.5, 0.5, 0.0, 1.0, 0.0, 0.0,//v0
+		 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,//v1
+		 0.5, 0.5, 0.0, 0.0, 0.0, 1.0,//v2 
+		//Triangulo 1
+		 0.0, 0.0, 0.0, 1.0, 1.0, 0.0,//v3
+		-0.5,-0.5, 0.0, 0.0, 1.0, 1.0,//v4
+		 0.5,-0.5, 0.0, 1.0, 0.0, 1.0,//v5 
+	};
+
+	GLuint VBO, VAO;
+	//Geração do identificador do VBO
+	glGenBuffers(1, &VBO);
+	//Faz a conexão (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//Envia os dados do array de floats para o buffer da OpenGl
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//                                              vertices.data()  
+	//Geração do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// e os ponteiros para os atributos 
+	glBindVertexArray(VAO);
+	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
+	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
+	// Tipo do dado
+	// Se está normalizado (entre zero e um)
+	// Tamanho em bytes 
+	// Deslocamento a partir do byte zero 
+	
+	//Atributo 0 - posição
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//Atributo 1 - cor
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 
 	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
 	// atualmente vinculado - para que depois possamos desvincular com segurança
